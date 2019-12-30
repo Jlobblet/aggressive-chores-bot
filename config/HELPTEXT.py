5903ebf214bdfcf4ec6e9b79146e27055659a427
Jlobblet <37539527+Jlobblet@users.noreply.github.com>
2019-12-30 23:50:43 +0000

    Add helptext

--- config/HELPTEXT.py
+++ config/HELPTEXT.py
@@ -1,6 +1,18 @@
 HELPTEXT = {
-    "show_chores": {"help": "", "brief": ""},
-    "add_chore": {"help": "", "brief": ""},
-    "remove_chore": {"help": "", "brief": ""},
-    "complete_chore": {"help": "", "brief": ""},
+    "show_chores": {
+        "help": 'Followed by either "all" or "my". "all" shows all uncompleted chores on this server, "my" shows only the caller\'s.',
+        "brief": "Show outstanding chores.",
+    },
+    "add_chore": {
+        "help": "Followed by a mention and then the description of the chore. Adds a chore assigned to a user.",
+        "brief": "Create new chores.",
+    },
+    "remove_chore": {
+        "help": "Followed by id number to delete. Deletes a chore from the database.",
+        "brief": "Delete chores.",
+    },
+    "complete_chore": {
+        "help": "Followed by id number. Mark an existing chore as complete.",
+        "brief": "Mark chores as complete.",
+    },
 }
