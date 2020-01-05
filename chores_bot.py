e5b79c81b1b2b874a6226dd1deadcdbfd3c5ee60
Jlobblet <37539527+Jlobblet@users.noreply.github.com>
2020-01-05 13:01:08 +0000

    Move reaction listening to its own script
    Also rename commands/chores/subcommands/ to
    commands/chores/shared/ to better reflect its purpose

--- chores_bot.py
+++ chores_bot.py
@@ -12,7 +12,6 @@ from utils.utils import (
     set_admin,
     del_messages,
 )
-from commands.chores.subcommands.complete_chore import complete_chore
 from config.CONFIG import CONFIG
 from config.DISCORD import DISCORD_SECRET
 
@@ -22,6 +21,7 @@ extensions = [
     "commands.chores.complete_chore",
     "commands.chores.remove_chore",
     "commands.chores.show_chores",
+    "commands.chores.reaction_listener",
     "commands.admin",
 ]
 bot = Bot(command_prefix=prefix)
@@ -52,34 +52,6 @@ async def on_ready():
         run_file_format("sql/add_guild.sql", guild_id=guild)
     print("...done")
     await bot.change_presence(activity=discord.Game(f"{prefix}help"))
-
-
-@bot.event
-async def on_reaction_add(reaction, user):
-    if not user.bot:
-        message_id = reaction.message.id
-        channel_id = reaction.message.channel.id
-        guild_id = reaction.message.guild.id
-        message_data = run_file_format(
-            "sql/find_message.sql", message_id=message_id, channel_id=channel_id
-        )
-        chore_id = message_data[0]["chore_id"]
-        chore_data = run_file_format(
-            "sql/find_chore.sql", guild_id=guild_id, chore_id=chore_id
-        )[0]
-        asignee_id = chore_data["user_id"]
-        creator_id = chore_data["creator"]
-        if reaction.emoji == "✅" and user.id == asignee_id:
-            await complete_chore(reaction, user.id, chore_id)
-        elif reaction.emoji == "🗑️" and (
-            check_admin(user.id, reaction.message.guild.id) or creator_id == user.id
-        ):
-            run_file_format(
-                "sql/remove_chore.sql", guild_id=guild_id, chore_id=chore_id
-            )
-        else:
-            return None
-        await del_messages(bot, guild_id, chore_id)
 
 
 if __name__ == "__main__":
